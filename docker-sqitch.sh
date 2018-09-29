bf326e106d1dd321b3d52b3a632f5616d1940081
David E. Wheeler <david.wheeler@iovation.com>
2018-09-29 17:45:24 -0400

    Update ORIG environment vars

--- docker-sqitch.sh
+++ docker-sqitch.sh
@@ -11,8 +11,8 @@ docker run -it \
     --network host \
     --mount "type=bind,src=$(pwd),dst=/repo" \
     --mount "type=bind,src=$HOME,dst=/home" \
-    -e "SQITCH_USER=$user" \
-    -e "SQITCH_ORIG_NAME=$fullname" \
+    -e "SQITCH_ORIG_SYSUSER=$user" \
+    -e "SQITCH_ORIG_FULLNAME=$fullname" \
     -e "SQITCH_ORIG_EMAIL=$user@$(hostname)" \
     -e "TZ=$(date +%Z)" \
     -e "LESS=${LESS:--R}" \
